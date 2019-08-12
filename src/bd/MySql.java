ad71d73f0a2a76d70eba928223c5210b9acf7852
anorpe <aorregop@unal.edu.co>
2019-08-11 23:35:07 -0500

    Tablas MySql

---
+++ src/bd/MySql.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+import agentes.AgenteInterfaz;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author andres
+ */
+public class MySql {
+    
+    
+    public static String database = "facerecognition";
+    public static String user = "root";
+    public static String password = "anorpemysql";
+    public static Connection conexion = null;
+    
+    public static void Conectar(){
+        try {
+                Class.forName("com.mysql.jdbc.Driver");
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(AgenteInterfaz.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        
+  
+                try {
+                    conexion = DriverManager.getConnection("jdbc:mysql://localhost/"+database,user, password);
+                } catch (SQLException ex) {
+                    Logger.getLogger(AgenteInterfaz.class.getName()).log(Level.SEVERE, null, ex);
+                }
+    }
+    
+    
+    public static void ejecutar(String Query){
+        
+        
+        //String Query = "INSERT INTO usuario VALUES ("+cedula+",'"+nombre+"','"+role+"','"+facultad+"')";
+        //System.out.println(Query);
+        Statement st = null;
+        try {
+            st = conexion.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(AgenteInterfaz.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            st.execute(Query);
+        } catch (SQLException ex) {
+            Logger.getLogger(AgenteInterfaz.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+
+
+    
+}
---
+++ tablas.sql
@@ -0,0 +1,65 @@
+DROP TABLE asignacion;
+DROP TABLE salon;
+DROP TABLE usuario;
+DROP TABLE facultad;
+
+
+CREATE TABLE facultad(
+	nombre VARCHAR(20) PRIMARY KEY
+);
+
+
+
+
+CREATE TABLE usuario(
+	cedula INT PRIMARY KEY,
+	nombre VARCHAR(20),
+	rol VARCHAR(20),
+	facultad VARCHAR(20),
+	FOREIGN KEY(facultad) REFERENCES facultad(nombre)
+	
+);
+
+
+
+CREATE TABLE salon(
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	numero INT,
+	bloque INT,
+	facultad VARCHAR(20),
+	FOREIGN KEY(facultad) REFERENCES facultad(nombre)
+);
+
+
+CREATE TABLE asignacion(
+
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	dia VARCHAR(20), 
+	hora VARCHAR(20),
+	salon INT UNSIGNED,
+	usuario INT,
+	FOREIGN KEY(salon) REFERENCES salon(id),
+	FOREIGN KEY(usuario) REFERENCES usuario(cedula)
+);
+
+
+INSERT INTO facultad VALUES ('Minas');
+INSERT INTO facultad VALUES ('Ciencias Exactas');
+INSERT INTO facultad VALUES ('Ciencias Humanas');
+
+
+INSERT INTO usuario VALUES (1,'Andres','Estudiante','Minas');
+INSERT INTO usuario VALUES (2,'Jose','Estudiante','Minas');
+INSERT INTO usuario VALUES (3,'Haisson','Estudiante','Minas');
+
+INSERT INTO salon VALUES (1,101,03,'Minas');
+INSERT INTO salon VALUES (2,102,03,'Minas');
+INSERT INTO salon VALUES (3,101,03,'Ciencias Exactas');
+
+INSERT INTO asignacion VALUES (1,'Lunes',6,1,1);
+INSERT INTO asignacion VALUES (2,'Lunes',8,1,2);
+INSERT INTO asignacion VALUES (3,'Lunes',10,1,3);
+INSERT INTO asignacion VALUES (4,'Lunes',12,1,1);
+
+
+
